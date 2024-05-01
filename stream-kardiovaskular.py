d0bc66c4563d1a883ee1853c7dfc1b71f32c3978
Vera Artanti <103916920+veraartanti@users.noreply.github.com>
2024-05-01 18:25:32 +0700

    Update stream-kardiovaskular.py

--- stream-kardiovaskular.py
+++ stream-kardiovaskular.py
@@ -2,37 +2,43 @@ import pickle
 import numpy as np
 import streamlit as st
 
-# Judul
+# load saved model
+model = pickle.load(open('kardiovaskular.sav', 'rb'))
+
+# judul web
 st.title('Klasifikasi Penyakit Kardiovaskular')
 
-# Deskripsi
-st.markdown("""
-Aplikasi ini melakukan klasifikasi pasien berdasarkan beberapa fitur untuk memprediksi apakah pasien menderita penyakit kardiovaskular atau tidak.
-""")
+# Konten sidebar
+st.sidebar.header('Pencegahan Penyakit Kardiovaskular:')
+st.sidebar.markdown('- Hindari merokok')
+st.sidebar.markdown('- Tetap aktif secara fisik')
+st.sidebar.markdown('- Konsumsi makanan sehat dan seimbang')
+st.sidebar.markdown('- Pantau tekanan darah secara teratur')
+st.sidebar.markdown('- Batasi konsumsi alkohol')
+st.sidebar.markdown('- Kelola stres dengan baik')
 
-# Sidebar
-st.sidebar.header('Pencegahan Penyakit Kardiovaskular')
-st.sidebar.markdown("""
-1. Makanlah makanan sehat seperti buah-buahan, sayuran, dan biji-bijian.
-2. Lakukan olahraga secara teratur.
-3. Hindari merokok dan konsumsi alkohol berlebihan.
-4. Pertahankan berat badan yang sehat.
-5. Atur tekanan darah dan kolesterol Anda.
-6. Cek kesehatan secara rutin.
-""")
+# Input data
+col1, col2, col3 = st.columns(3)
+with col1:
+    time = st.number_input('Time')
+    age = st.number_input('Age')
+    serum_sodium = st.number_input('Serum Sodium')
+with col2:
+    serum_creatinine = st.number_input('Serum Creatine')
+    creatinine_phosphokinase = st.number_input('Creatine Phosphokinase')
+with col3:
+    ejection_fraction = st.number_input('Ejection Fraction')
+    platelets = st.number_input('Platelets')
 
-# Input fitur
-st.header('Masukkan Fitur Pasien')
-time = st.number_input('Time')
-serum_creatinine = st.number_input('Serum Creatine')
-ejection_fraction = st.number_input('Ejection Fraction')
-age = st.number_input('Age')
-serum_sodium = st.number_input('Serum Sodium')
-creatinine_phosphokinase = st.number_input('Creatine Phosphokinase')
-platelets = st.number_input('Platelets')
+# code for prediction
+heart_diagnosis =''
 
-# Prediksi
-if st.button('Klasifikasi Penyakit Kardiovaskular'):
-    # Lakukan prediksi di sini
-    prediction = "Pasien Meninggal"  # Ganti ini dengan hasil prediksi sesungguhnya
-    st.success(f'Hasil Prediksi: {prediction}')
+# membuat tombol prediksi
+if st.button('Cek Klasifikasi'):
+    heart_prediction = model.predict([[time, serum_creatinine, ejection_fraction, age, serum_sodium, creatinine_phosphokinase, platelets]])
+
+    if heart_prediction[0] == 1:
+        heart_diagnosis = 'Pasien Meninggal'
+    else:
+        heart_diagnosis = 'Pasien Tidak Meninggal'
+st.success(f'Hasil Klasifikasi: {prediction}')
